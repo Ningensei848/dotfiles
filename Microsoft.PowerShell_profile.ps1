42943c5d1b9eb104a917c5d8abb8edcbfed23326
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2019-02-28 11:02:51 +0900

    環境変数を分離して読み込めるようにスクリプトを切り出した

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,18 @@
-# My Powershell profile
-# cf. https://bit.ly/2txHxQw
-# cf. http://d.hatena.ne.jp/pshell/20140428/1398690933
+ # My Powershell profile
+ # cf. https://bit.ly/2txHxQw
+ # cf. http://d.hatena.ne.jp/pshell/20140428/1398690933
+ # cf. https://qiita.com/kkdd/items/ac6e65f6e2e8fc32e7ca (proxy, env)
+ # cf. https://bit.ly/2T7fbMn (reuse powershell Scripts)
+
+
+ # ######################### System Enviroment Variable ################################
+ # システム情報を取得し、「計算機の名前+現在のユーザ名」を取得し、ユーザ名だけを切り出す
+ $localuser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split("\")[1]
+
+ if (Test-Path "C:\Users\$($localuser)\Documents\WindowsPowerShell\ProxyEnviromentConfig.ps1") {
+
+     . "C:\Users\$($localuser)\Documents\WindowsPowerShell\ProxyEnviromentConfig.ps1"
+     }
 
 # Cheat sheet:
 # - `sal`: Set-Alias
@@ -24,7 +36,7 @@ function CustomUpdate {explorer ms-settings:windowsupdate}
 sal update CustomUpdate
 
 # ####### docker-machine #######
-function CustomStartDockerMachine { docker-machine ls; docker-machine restart default; docker-machine env; & "C:\Program Files\Docker Toolbox\docker-machine.exe" env | Invoke-Expression; docker-machine ls }
+function CustomStartDockerMachine { docker-machine ls; docker-machine restart default; docker-machine env; & docker-machine env | Invoke-Expression; docker-machine ls }
 sal start-docker CustomStartDockerMachine
 
 # ####### move to custom RootDIR #######
@@ -48,4 +60,3 @@ sal bld BubLog_DB_ROOT
 # ###### home ######
 function CustomCdHome {cd "C:\Users\kiai\myDevelopment"}
 sal home CustomCdHome
-
